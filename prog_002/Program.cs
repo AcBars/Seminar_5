496a2998e8b4346ddaf58bc983fe3eefb568d73a
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-23 00:11:51 +0300

    task_33 ver.2 working

--- prog_002/Program.cs
+++ prog_002/Program.cs
@@ -9,25 +9,38 @@
 
 using static System.Console;
 Clear();
+int [] array = RandomArray (5,-5,5);
 WriteLine("Введите число");
 int number=int.Parse(ReadLine());
-int [] array=GetRandomArray(5,-5,5);
-WriteLine($"{number}; массив [{String.Join("," array)});
+//WriteLine($"[{String.Join(", ", array)}]");
+FaindArray(array, number);
 
-//int[] array= [6, 7, 19, 345, 3];
 
-int[] GetRandomArray(int size, int minValue, int maxValue)
+
+
+
+
+int [] RandomArray(int size,  int minValue, int maxValue)
 {
-    
+    int [] arr = new int [size];
+    for(int i=0; i<size; i++)
+    {
+        arr [i]=new Random().Next(minValue, maxValue+1);
+    }
+    return arr;
 }
-
-bool FindNumber(int[] array, int numbers)
+void FaindArray (int [] array, int number)
 {
+    int count =0;
     for(int i=0; i<array.Length; i++)
     {
-        if (array[i]==numbers)
+        if (number==array[i])
         {
-
+            count++;
+            //
+            //break;
         }
     }
+      WriteLine(count==0?$"Числа {number} нет в массиве [{String.Join(", ", array)}]."
+      : $"Число {number} есть в массиве [{String.Join(", ", array)}].");
 }
